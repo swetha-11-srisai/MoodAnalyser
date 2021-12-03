533a89c5aabd0086afe9796947413a29d0918fc6
swetha-11-srisai <swethachowdary857@gmail.com>
2021-12-03 08:06:56 -0800

    [Swetha] add :respond to mood

---
+++ MoodAnalyser/MoodTesting/MoodAnalyserCode.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MoodTesting
+{
+    internal class MoodAnalyserCode
+    {
+        internal string AnalyseMood(string v)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
